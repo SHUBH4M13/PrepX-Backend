7b24dfa62279119cc4c08d983ec371b19acfdf94
Shubham Karna <skarna230@gmail.com>
2025-06-12 17:12:41 +0530

    system

--- src/test/java/com/XGroup/PrepX/Backend/PrepXBackendApplication.java
+++ src/test/java/com/XGroup/PrepX/Backend/PrepXBackendApplication.java
@@ -3,10 +3,11 @@ package com.XGroup.PrepX.Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public  class PrepXBackendApplication {
 
-	public static void main( String args[]){
+	public static void main(String[] args){
 		SpringApplication.run(PrepXBackendApplication.class, args);
 	}
 
