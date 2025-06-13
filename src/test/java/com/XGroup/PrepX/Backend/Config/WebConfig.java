6e32279d02193c9ada56011e0b8de4846cbe0b48
Shubham Karna <150832133+SHUBH4M13@users.noreply.github.com>
2025-06-13 14:40:44 +0530

    Update WebConfig.java

--- src/test/java/com/XGroup/PrepX/Backend/Config/WebConfig.java
+++ src/test/java/com/XGroup/PrepX/Backend/Config/WebConfig.java
@@ -1,19 +1,1 @@
-package com.XGroup.PrepX.Backend.Config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://prep-x-blush.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
